d87be5eda7a478981fd1562cacc197c3632cb07e
nintendogamer15 <nintendogamer15@live.com>
2024-12-25 15:18:00 -0500

    Uhhh... Lots of stuff.

--- ClipCascade_Desktop/src/main.py
+++ ClipCascade_Desktop/src/main.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-
 # ClipCascade - A seamless clipboard syncing utility
 # Repository: https://github.com/Sathvik-Rao/ClipCascade
 #
 # Author: Sathvik Rao Poladi
 # License: GPL-3.0
 #
-# This script serves as the entry point for the ClipCascade application,
+# This script serves as the entry point for the ClipCascade application
 # initializing and running the core application logic.
+
+import os
+import sys
+
+# Add Flatpak-specific module path
+if os.path.exists('/app/lib/python3.11/site-packages'):
+    sys.path.insert(0, '/app/lib/python3.11/site-packages')
 
 from core.application import Application
 
